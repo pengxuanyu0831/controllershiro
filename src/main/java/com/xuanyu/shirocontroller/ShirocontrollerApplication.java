882041cc5855b685582ae3ac5373404dc9e3b4da
pengxy <pengxuanyus@gmail.com>
2020-06-20 23:22:16 +0800

    springboot 做一个权限控制系统，用shiro

---
+++ src/main/java/com/xuanyu/shirocontroller/ShirocontrollerApplication.java
@@ -0,0 +1,13 @@
+package com.xuanyu.shirocontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShirocontrollerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShirocontrollerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xuanyu/shirocontroller/ShirocontrollerApplicationTests.java
@@ -0,0 +1,15 @@
+package com.xuanyu.shirocontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.stereotype.Controller;
+
+@Controller
+class ShirocontrollerApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("hello spring boot");
+    }
+
+}
